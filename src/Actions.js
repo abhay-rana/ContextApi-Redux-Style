be74d0fc94e32d0097819db372fb334ec13cc06b
Abhay Rana <abhay.rana@healthmug.com>
2022-07-17 15:33:47 +0530

    actions converted into the arrow functions

--- src/Actions.js
+++ src/Actions.js
@@ -1,22 +1,14 @@
-export function IncreaseAction(dispatch) {
-	// return {
-	// 	type: "INCREMENT",
-	// };
-	dispatch({ type: "INCREMENT" });
-}
-export function DecreaseFunction(dispatch) {
-	dispatch({ type: "DECREMENT" });
-}
-export async function GetDetails(dispatch) {
+export const IncreaseAction = (dispatch) => dispatch({ type: "INCREMENT" });
+export const DecreaseFunction = (dispatch) => dispatch({ type: "DECREMENT" });
+export const GetDetails = async (dispatch) => {
 	const res = await fetch("https://api.github.com/users/abhay-rana");
 	const data = await res.json();
 	console.log(data.login, "action");
 	return dispatch({ type: "GETDETAILS", data: data.login });
-}
+};
 
-export function SetDetails(dispatch, name) {
+export const SetDetails = (dispatch, name) =>
 	dispatch({
 		type: "SETNAME",
 		data: name,
 	});
-}
